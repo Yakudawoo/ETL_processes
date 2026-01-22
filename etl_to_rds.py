7fa377266974d12f0f0f6feb79c5961bb5a42b2e
Yakudawoo <olivierkdw@gmail.com>
2026-01-22 16:47:04 +0100

    push automatique nok

--- etl_to_rds.py
+++ etl_to_rds.py
@@ -53,12 +53,11 @@ df = df_final
 # 2. CHARGEMENT DANS POSTGRES RDS
 # ---------------------------
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME")
-
+DB_USER = os.getenv("PG_USER")
+DB_PASSWORD = os.getenv("PG_PASSWORD")
+DB_HOST = os.getenv("PG_HOST")
+DB_PORT = os.getenv("PG_PORT", "5432")
+DB_NAME = os.getenv("PG_DATABASE")
 
 engine = create_engine(
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
